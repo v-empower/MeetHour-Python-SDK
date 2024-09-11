cdef8aeec668a5c2d70d36a4faebad7551f544a4
Shoeb Ahmad Fareed <shoeb.ahmad@v-empower.com>
2024-09-11 14:23:48 +0530

    Update key.py
    Removing keys

--- src/pymeethour/webhook/key.py
+++ src/pymeethour/webhook/key.py
@@ -1,4 +1,4 @@
 #Webhooks Credentials - Signup for Meet Hour Developer Plan (https://meethour.io)
 #Go to Webhooks settings and enable the webhooks
 
-SECRET_KEY = '690a5d266be736e350467bc7c6aeb02494023e7c5ad360fcf5e4a1d0a2f3bde5'  # Replace with your actual secret key
+SECRET_KEY = ''  # Replace with your actual secret key
